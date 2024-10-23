cb4d46fe404395e36d70042ef0788bb5d744c648
Kim Nejudne <SNejudne@fabletics.com>
2024-10-23 21:01:39 +0800

    copy contents of namespace.js to slack.js

---
+++ socketio201/slackClone/slack.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+const io = socketio(expressServer)
+
+io.on("/").on('connection',(socket)=>{
+    console.log(socket.id,"has connected")
+    socket.on('newMessageToServer',(dataFromClient)=>{
+        console.log("Data:",dataFromClient);
+        io.emit('newMessageToClients',{text:dataFromClient.text});
+    })
+})
+

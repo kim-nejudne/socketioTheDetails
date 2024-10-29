c0a233a962a67cd4ed9f80cadeb6a1d800080961
Kim Nejudne <SNejudne@fabletics.com>
2024-10-29 21:28:46 +0800

    rooms practice

--- socketio201/namespaces/namespaces.js
+++ socketio201/namespaces/namespaces.js
@@ -1,26 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // require('socket.io') = Server in the docs
-const socketio = require('socket.io')
+const socketio = require("socket.io");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(8001);
 // io = the server object in the docs!
-const io = socketio(expressServer)
+const io = socketio(expressServer);
 
 // io = server in the docs
-io.of("/").on('connection',(socket)=>{
-    console.log(socket.id,"has connected")
-    //in ws we use "send" method, and it socket.io we use the "emit" method
-    // socket.emit('messageFromServer',{data:"Welcome to the socket server!"})
-    socket.on('newMessageToServer',(dataFromClient)=>{
-        console.log("Data:",dataFromClient);
-        io.emit('newMessageToClients',{text:dataFromClient.text});
-    })
-})
+io.of("/").on("connection", (socket) => {
+  socket.join("chat");
+  io.of("/")
+    .to("chat")
+    .to("chat2")
+    .to("chat3")
+    .emit("chatRoomWelcome", { stuff: "some stuff" });
+  console.log(socket.id, "has connected");
+  //in ws we use "send" method, and it socket.io we use the "emit" method
+  // socket.emit('messageFromServer',{data:"Welcome to the socket server!"})
+  socket.on("newMessageToServer", (dataFromClient) => {
+    console.log("Data:", dataFromClient);
+    io.emit("newMessageToClients", { text: dataFromClient.text });
+  });
+});
 
-io.of("/admin").on('connection',(socket)=>{
-    console.log("Someone connected to the admin namespace!")
-    io.of("/admin").emit('admin-welcome',"Welcome to the admin channel, INFIDELS!")
+io.of("/admin").on("connection", (socket) => {
+  socket.join("adminChat");
+  io.of("/admin").to("adminChat").emit("adminChatWelcome", "Welcome to the admin chat!");
+
+  console.log("Someone connected to the admin namespace!");
+  io.of("/admin").emit(
+    "admin-welcome",
+    "Welcome to the admin channel, INFIDELS!"
+  );
 });
